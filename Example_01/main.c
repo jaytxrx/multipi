ea278fd0d8a99038354c243469a8d42e75aa04d8
Jeffrey Antony <jeffreyantony@gmail.com>
2015-10-21 20:01:46 +0200

    pretified the code

--- Example_01/main.c
+++ Example_01/main.c
@@ -1,7 +1,13 @@
 #include <stdint.h>
 
+#define RPI RPI2
+
 #include "memio.h"
 #include "lib.h"
+
+#if(RPI == RPI1)
+    #error "Example works only for Raspberry Pi 2"
+#endif
 
 #define ENABLE_RED_LED()    ARM_GPIO_GPFSEL1 |= 1 << 18; /*GPIO16(GPIO Function Select 1)*/
 #define ENABLE_YELLOW_LED() ARM_GPIO_GPFSEL2 |= 1 << 00; /*GPIO20(GPIO Function Select 2)*/
@@ -20,138 +26,78 @@ void core3_main(void) __attribute__((naked));
 void core1_main(void) __attribute__((naked));
 void core2_main(void) __attribute__((naked));
 
-
 void core0_submain(void) __attribute__((naked));
 
-void main(void) /*belongs to Core 0*/
+void main(void)
 {
     enable_JTAG();
-            //ENABLE_ACT_LED();
-                    //ACT_LED_OFF();
-             ENABLE_YELLOW_LED();
-         
-                  //  ENABLE_GREEN_LED();
+
+    ENABLE_ACT_LED();
     
-   // if(get_core_number() == 4)
-    {
-    // start_core1(core1_main);
-         //start_core2(core2_main);
-   // start_core3(core3_main);
+    ENABLE_YELLOW_LED();
+    ENABLE_GREEN_LED();
+    ENABLE_RED_LED();
 
-    
-   
- 
-           // core0_submain();
-    }
-    
-            volatile uint32_t i;
-    
-    while(1)
-    {
+    start_core1(core1_main);
+    start_core3(core3_main);
+    start_core2(core2_main);
 
-                for(i=0; i < 300000; i++); //wait for some time
-        YELLOW_LED_ON();
-   for(i=0; i < 300000; i++); //wait for some time
-
-
-
-      YELLOW_LED_OFF();
-
-
-     
-
-    }
-    
-    
-
+    core0_submain();
 }
 
 
 void core0_submain(void)
 {
-/*
-
     volatile uint32_t i;
 
     while(1)
     {
         for(i=0; i < 500000; i++); //wait for some time
-     //   ACT_LED_ON();
-
-        
+        ACT_LED_ON();
         for(i=0; i < 500000; i++); //wait for some time
-
         ACT_LED_OFF();
-
-
-    }*/
-    
-
+    }
 }
 
 
 void core1_main(void)
 {
-            
-
-
+    volatile uint32_t i;
     
-
-
+    while(1)
+    {
+        for(i=0; i < 300000; i++); //wait for some time
+        YELLOW_LED_ON();
+        for(i=0; i < 300000; i++); //wait for some time
+        YELLOW_LED_OFF();
+    }
 }
 
 void core2_main(void)
 {
-    
-         /*
-       ENABLE_RED_LED();
-    
     volatile uint32_t i;
     
     while(1)
     {
-        
-        
         for(i=0; i < 200000; i++); //wait for some time
         RED_LED_ON();
-
-   for(i=0; i < 200000; i++); //wait for some time
-
+        for(i=0; i < 200000; i++); //wait for some time
         RED_LED_OFF();
-
-
-     
-
     }
-    
-*/
-   
-
 }
 
 void core3_main(void)
 {
-
-/*
-
-    
     volatile uint32_t i;
     
     while(1)
     {
         
         for(i=0; i < 500000; i++); //wait for some time
-
         GREEN_LED_ON();
-
-
         for(i=0; i < 500000; i++); //wait for some time
-
         GREEN_LED_OFF();
-
-
     }
-*/
 }
 
 
